077a4edcd30307213fa40665f3dc603365f7e917
Nxusbets <169100845+Nxusbets@users.noreply.github.com>
2024-07-26 16:47:29 +0000

    cambio final

--- src/app.js
+++ src/app.js
@@ -1,11 +1,31 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+// app.js
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let who = ["Mi perro", "Mi hermana", "Mi vecino", "Mi jefe", "El cartero"];
+let action = ["se comió", "rompió", "perdió", "tiró", "olvidó"];
+let what = ["mi tarea", "mi llave", "mi teléfono", "mis deberes", "mi informe"];
+let when = [
+  "ayer por la noche",
+  "esta mañana",
+  "justo antes de salir",
+  "mientras dormía",
+  "mientras estaba de vacaciones"
+];
+
+function generarExcusa() {
+  let quien = who[Math.floor(Math.random() * who.length)];
+  let accion = action[Math.floor(Math.random() * action.length)];
+  let que = what[Math.floor(Math.random() * what.length)];
+  let cuando = when[Math.floor(Math.random() * when.length)];
+
+  return `${quien} ${accion} ${que} ${cuando}.`;
+}
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let generateButton = document.getElementById("generateButton");
+  let excuseElement = document.getElementById("excuse");
+
+  generateButton.addEventListener("click", function() {
+    let excusaGenerada = generarExcusa();
+    excuseElement.textContent = excusaGenerada;
+  });
 };
